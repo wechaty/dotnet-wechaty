87926ced2a23303dd82e33a70c8b6d7ca626f0c8
Darren <jesn2013@hotmail.com>
2020-12-10 12:05:17 +0800

    add filebox JsonProperty attr

--- src/external-modules/Wechaty.FileBox/FileBoxJsonObjectQRCode.cs
+++ src/external-modules/Wechaty.FileBox/FileBoxJsonObjectQRCode.cs
@@ -1,9 +1,15 @@
-﻿
+
+using Newtonsoft.Json;
+
 namespace Wechaty
 {
     public class FileBoxJsonObjectQRCode : FileBoxJsonObject
     {
+
+        [JsonProperty("boxType")]
         public override FileBoxType BoxType => FileBoxType.QRCode;
+
+        [JsonProperty("qrCode")]
         public string QrCode { get; set; }
     }
 }
