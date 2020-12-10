c1b331daf59e769908a837b379ebd85aa8928d86
Darren <jesn2013@hotmail.com>
2020-12-10 12:07:58 +0800

    add filebox JsonProperty attr

--- src/external-modules/Wechaty.Puppet/Schemas/UrlLink.cs
+++ src/external-modules/Wechaty.Puppet/Schemas/UrlLink.cs
@@ -1,10 +1,20 @@
+using Newtonsoft.Json;
+
 namespace Wechaty.Schemas
 {
     public class UrlLinkPayload
     {
+
+        [JsonProperty("description", Required = Required.Always)]
         public string? Description { get; set; }
+
+        [JsonProperty("thumbnailUrl", Required = Required.Always)]
         public string? ThumbnailUrl { get; set; }
+
+        [JsonProperty("title", Required = Required.Always)]
         public string Title { get; set; }
+
+        [JsonProperty("url", Required = Required.Always)]
         public string Url { get; set; }
     }
 }
