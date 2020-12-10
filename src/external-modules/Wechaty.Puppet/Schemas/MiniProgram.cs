ab1c6a3686a0c1556cbfddd5fef7f46540aca69f
Darren <jesn2013@hotmail.com>
2020-12-10 12:07:58 +0800

    add filebox JsonProperty attr

--- src/external-modules/Wechaty.Puppet/Schemas/MiniProgram.cs
+++ src/external-modules/Wechaty.Puppet/Schemas/MiniProgram.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace Wechaty.Schemas
 {
     public class MiniProgramPayload
@@ -5,38 +7,47 @@ namespace Wechaty.Schemas
         /// <summary>
         /// optional, appid, get from wechat (mp.weixin.qq.com)
         /// </summary>
+        [JsonProperty("appid", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? Appid { get; set; }
         /// <summary>
         /// optional, mini program title
         /// </summary>
+        [JsonProperty("description", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? Description { get; set; }
         /// <summary>
         /// optional, mini program page path
         /// </summary>
+        [JsonProperty("pagePath", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? PagePath { get; set; }
         /// <summary>
         /// optional, mini program icon url
         /// </summary>
+        [JsonProperty("iconUrl", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? IconUrl { get; set; }
         /// <summary>
         /// optional, the unique userId for who share this mini program
         /// </summary>
+        [JsonProperty("shareId", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? ShareId { get; set; }
         /// <summary>
         /// optional, default picture, convert to thumbnail
         /// </summary>
+        [JsonProperty("thumbUrl", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? ThumbUrl { get; set; }
         /// <summary>
         /// optional, mini program title
         /// </summary>
+        [JsonProperty("title", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? Title { get; set; }
         /// <summary>
         /// original ID, get from wechat (mp.weixin.qq.com)
         /// </summary>
+        [JsonProperty("username", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? Username { get; set; }
         /// <summary>
         /// original, thumbnailurl and thumbkey will make the headphoto of mini-program better
         /// </summary>
+        [JsonProperty("thumbKey", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string? ThumbKey { get; set; }
     }
 
