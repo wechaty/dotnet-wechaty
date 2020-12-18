31523ad698d9d1c2ca182806fa182b6e17704de6
Darren <jesn2013@hotmail.com>
2020-12-18 14:06:23 +0800

    update nuget package as  0.1.4

--- nupkg/push_packages.ps1
+++ nupkg/push_packages.ps1
@@ -5,15 +5,13 @@ $apiKey = $args[0]
 Write-Output $apiKey;
 
 # Get the version
-[xml]$commonPropsXml = Get-Content (Join-Path $rootFolder "src/common.props")
-$version = $commonPropsXml.Project.PropertyGroup.Version
-
-Write-Output $projects
+[xml]$commonPropsXml = Get-Content (Join-Path $rootFolder "src/Directory.Build.props")
+[string]$version = $commonPropsXml.Project.PropertyGroup.Version
 
 # Publish all packages
 foreach($project in $projects) {
     $projectName = $project.Substring($project.LastIndexOf("/") + 1)
-    $packagePath= Join-Path "./package" ($projectName + "." + $version + ".nupkg")
+    $packagePath= Join-Path "./package" ($projectName + "." + $version.Trim() + ".nupkg")
     Write-Output $packagePath
     dotnet nuget push $packagePath -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
 }
